aaf5c5f5520541e2914ded548a354299d0b7c1fa
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2019-09-22 22:10:21 +0100

    Document helper functions.
    Add docstrings to all helper functions.

--- pdcsl-lib/src/main/resources/qmul/pdcsl/helper.py
+++ pdcsl-lib/src/main/resources/qmul/pdcsl/helper.py
@@ -2,6 +2,8 @@
 # Helper.py - Common helper functions
 # Copyright © 2019 Damien Goutte-Gattat
 
+"""Common helper functions for ImageJ."""
+
 from ij import ImagePlus
 from ij.plugin import ImageCalculator
 from ij.plugin.filter import Binary
@@ -9,7 +11,13 @@ from ij.process import AutoThresholder, ImageProcessor
 
 
 def apply_binary_filters(filters, image):
-    """Apply the named binary filters sequentially to the image."""
+    """Apply the named binary filters sequentially to the image.
+
+    :param filters: An array of filter names
+    :type filters: [str]
+    :param image: The image to apply the filter to
+    :type image: ij.ImagePlus
+    """
     bf = Binary()
     for name in filters:
         bf.setup(name, image)
@@ -17,6 +25,17 @@ def apply_binary_filters(filters, image):
 
 
 def auto_threshold(image, method, name=None):
+    """Apply an automatic threshold to an image.
+
+    :param image: The image to process
+    :type image: ij.ImagePlus
+    :param method: The thresholding method to use
+    :type method: ij.process.AutoThresholder.Method
+    :param name: The name to give to the resulting image
+        (default is the name of the original image + " Mask")
+    :returns: The thresholded image
+    :rtype: ij.ImagePlus
+    """
     thresholder = AutoThresholder()
     ip = image.getProcessor()
     threshold = thresholder.getThreshold(method, ip.getHistogram())
@@ -30,6 +49,19 @@ def auto_threshold(image, method, name=None):
 
 
 def apply_mask(source, mask):
+    """Apply a binary mask to an image.
+
+    The mask is expected to contain to be a 8-bit binary image,
+    with black pixels (0) defining the regions to keep in the
+    masked image.
+
+    :param source: The source image to apply the mask to
+    :type source: ij.process.ImageProcessor
+    :param mask: The image containing the mask to apply
+    :type mask: ij.process.ImageProcessor
+    :returns: The masked image
+    :rtype: ij.process.ImageProcessor
+    """
     calc = ImageCalculator()
     source_image = ImagePlus("src", source.duplicate())
     mask_image = ImagePlus("mask", mask.duplicate())
