73285dce1ba01dd052c6126605c398ff29e65bc7
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2019-10-06 22:03:21 +0100

    Simplify  OC_Volume_Counter script.
    Re-write the OC_Volume_Counter script using the helper functions
    now available in pdcsl-lib.

--- pdcsl-scripts/src/main/resources/scripts/Plugins/PDCSL/OC_Volume_Counter.py
+++ pdcsl-scripts/src/main/resources/scripts/Plugins/PDCSL/OC_Volume_Counter.py
@@ -1,36 +1,21 @@
-# OCMasker - Create masks for OncoChrome-positive regions
+# OC_Volume_Counter - Compute volume occupied by OncoChrome fluorophores
 # Copyright © 2019 Damien Goutte-Gattat
 
 from ij import IJ
+from qmul.pdcsl.helper import extract_volumes
 from qmul.pdcsl.oncochrome import create_masks
 
-def get_black_pixels_count(ip):
-    width = ip.getWidth()
-    height = ip.getHeight()
-    pixels = ip.getIntArray()
-    n = 0
-
-    for x in range(width):
-        for y in range(height):
-            if pixels[x][y] == 0:
-                n += 1
-    return n
 
 def run_script():
     img = IJ.getImage()
-    info = img.getFileInfo()
-    voxel = info.pixelWidth * info.pixelHeight * info.pixelDepth
 
     masks = create_masks(img)
+    masks.setCalibration(img.getCalibration())
     masks.show()
-    values = [0.0, 0.0, 0.0, 0.0]
 
-    for i in range(4):
-        for j in range(img.getNSlices()):
-            masks.setPosition(i+1, j+1, 1)
-            n = get_black_pixels_count(masks.getProcessor())
-            values[i] = values[i] + (n * voxel)
+    values = extract_volumes(masks, range(4))
 
-    IJ.log("{},{:.2f},{:.2f},{:.2f},{:.2f}".format(img.getTitle(),values[0], values[1], values[2], values[3]))
+    IJ.log("{},{:.2f},{:.2f},{:.2f},{:.2f}".format(img.getTitle(), *values))
+
 
 run_script()
