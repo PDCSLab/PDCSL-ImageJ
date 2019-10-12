3978b15815100f28b191dd7ea084e6c636c1d64a
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2019-10-12 21:08:35 +0100

    Remove the Nuclei_Counter_Stack script.
    Merge the Nuclei_Counter_Stack script into Nuclei_Counter, which
    can now process either a single processor or a full stack.

--- pdcsl-scripts/src/main/resources/scripts/Plugins/PDCSL/Nuclei_Counter.py
+++ pdcsl-scripts/src/main/resources/scripts/Plugins/PDCSL/Nuclei_Counter.py
@@ -1,3 +1,8 @@
+# NucleiCounter - Count cell nuclei
+# Copyright © 2019 Damien Goutte-Gattat
+
+#@ Boolean (label="Process entire stack", default=true) do_stack
+
 from ij import IJ, ImagePlus
 from ij.measure import ResultsTable
 from ij.plugin.filter import ParticleAnalyzer
@@ -5,17 +10,35 @@ from ij.plugin.filter import ParticleAnalyzer
 from qmul.pdcsl.features import NucleiSegmenter
 
 
-def run_script():
-    img = IJ.getImage()
+def run_script(do_stack):
+    image = IJ.getImage()
     segmenter = NucleiSegmenter()
     results = ResultsTable()
     analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_OUTLINES | ParticleAnalyzer.DISPLAY_SUMMARY,
                                 0, results, 0.0, 500.0, 0.0, 1.0)
 
-    segmented = ImagePlus("binary", segmenter.segment(img.getProcessor()))
-    segmented.show()
-    if analyzer.analyze(segmented):
-        output = analyzer.getOutputImage()
-        output.show()
+    if do_stack:
+        segmented = segmenter.segment_hyperstack(image, channels='all')
+        counter = 0
+        values = [0 for n in segmented.getNChannels()]
 
-run_script()
+        for i in range(segmented.getNChannels()):
+            for j in range(segmented.getNSlices()):
+                segmented.setPosition(i + 1, j + 1, 1)
+
+                if analyzer.analyze(segmented):
+                    n = rt.getCounter() - counter
+                    counter = rt.getCounter()
+                    values[i] += n
+
+        fmt = "{}" + ",{}" * len(values)
+        IJ.log(fmt.format(image.getTitle(), *values))
+    else:
+        segmented = ImagePlus("binary", segmenter.segment(image.getProcessor()))
+        segmented.show()
+        if analyzer.analyze(segmented):
+            output = analyzer.getOutputImage()
+            output.show()
+
+
+run_script(do_stack)
