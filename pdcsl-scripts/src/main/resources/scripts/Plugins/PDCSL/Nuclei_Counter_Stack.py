d0e956e59f7da692f9154ccf7319ccc360c97706
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2019-10-12 11:49:08 +0100

    Add the NucleiSegmenter class.
    Add a module intended to help with the analyze of object
    features in images. This module currently provides the
    NucleiSegmenter class, which implements a Watershed-based
    algorithm for cell nuclei segmentation.

--- pdcsl-scripts/src/main/resources/scripts/Plugins/PDCSL/Nuclei_Counter_Stack.py
+++ pdcsl-scripts/src/main/resources/scripts/Plugins/PDCSL/Nuclei_Counter_Stack.py
@@ -1,47 +1,32 @@
 from ij import IJ, ImagePlus
 from ij.measure import ResultsTable
-from ij.plugin.filter import EDM, GaussianBlur, ParticleAnalyzer
-from ij.process import AutoThresholder, ImageProcessor
+from ij.plugin.filter import ParticleAnalyzer
 
-channel_names = ['EGFP', 'mCitrine', 'mCherry', 'mTurquoise', 'Total']
+from qmul.pdcsl.features import NucleiSegmenter
+
 
 def run_script():
-	img = IJ.getImage()
-	nslices = img.getNSlices()
-	nchannels = img.getNChannels()
-	if nchannels != 5:
-		return
+    img = IJ.getImage()
+    segmenter = NucleiSegmenter()
+    results = ResultsTable()
+    analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE | ParticleAnalyzer.DISPLAY_SUMMARY,
+                                0, results, 0.0, 500.0, 0.0, 1.0)
 
-	gb = GaussianBlur()
-	thresholder = AutoThresholder()
-	edm = EDM()
+    segmented = segmenter.segment_hyperstack(img, channels='all')
+    counter = 0
+    values = [0 for n in segmented.getNChannels()]
 
-	rt = ResultsTable()
-	analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE | ParticleAnalyzer.DISPLAY_SUMMARY, 0, rt, 0.0, 500.0, 0.0, 1.0)
+    for i in range(segmented.getNChannels()):
+        for j in range(segmented.getNSlices()):
+            segmented.setPosition(i + 1, j + 1, 1)
 
-	counter = 0
+            if analyzer.analyze(segmented):
+                n = rt.getCounter() - counter
+                counter = rt.getCounter()
+                values[i] += n
 
-	values = [0,0,0,0,0]
-
-	for i in range(nchannels):
-		for j in range(nslices):
-			img.setPosition(i+1, j+1, 1)
-
-			t1 = img.getProcessor().duplicate()
-			gb.blurGaussian(t1, 2.0)
-			threshold = thresholder.getThreshold(AutoThresholder.Method.Moments, t1.getHistogram())
-			t1.setThreshold(threshold, 255, ImageProcessor.NO_LUT_UPDATE)
-			binary = ImagePlus("binary", t1.createMask())
-
-			edm.toWatershed(binary.getProcessor())
-			binary.getProcessor().invert()
-
-			if analyzer.analyze(binary):
-				n = rt.getCounter() - counter
-				counter = rt.getCounter()
-				values[i] += n
-
-	IJ.log("{},{},{},{},{},{}".format(img.getTitle(),values[0], values[1], values[2], values[3], values[4]))
+    fmt = "{}" + ",{}" * len(values)
+    IJ.log(fmt.format(img.getTitle(), *values))
 
 
 run_script()
