8ee20778d5ff587af84320d7777ee748a422f253
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2020-03-20 20:37:31 +0000

    Update the Foci_Counter script.
    Make sure that script behaves similarly to the batch version.

--- src/main/resources/scripts/Plugins/PDCSL/Foci_Counter.py
+++ src/main/resources/scripts/Plugins/PDCSL/Foci_Counter.py
@@ -1,5 +1,7 @@
+# @ ImagePlus img
+#
 # PDCSL-ImageJ - PDCS Lab’s ImageJ Collection
-# Copyright © 2019 Damien Goutte-Gattat
+# Copyright © 2020 Damien Goutte-Gattat
 #
 # Foci_Counter: Count H2A.X foci
 
@@ -7,30 +9,12 @@ from ij import IJ
 from ij.measure import ResultsTable
 from ij.plugin import ChannelSplitter, ZProjector
 from ij.plugin.filter import ParticleAnalyzer
-from ij.process import ImageProcessor
-
-
-def get_channel(img, fluorophore):
-    """Get the number of the channel for the specified fluorophore."""
-    info = img.getInfoProperty()
-    if not info:
-        return -1
-
-    for line in info.split('\n'):
-        if line.startswith('Information|Image|Channel|Fluor'):
-            key, value = line.split(' = ')
-            if value == fluorophore:
-                return int(key[-1]) - 1
-
-    return -1
+from ij.process import AutoThresholder, ImageProcessor
 
 
 def run_script():
     img = IJ.getImage()
-    ch = get_channel(img, 'Cy5')
-    if ch == -1:
-        # Cannot identify correct channel, assume last
-        ch = img.getNChannels() - 1
+    ch = img.getC()
 
     # Save any existing ROI
     roi = img.getRoi()
@@ -46,22 +30,21 @@ def run_script():
         channels = ChannelSplitter.split(ZProjector.run(img, 'max'))
     else:
         channels = ChannelSplitter.split(img)
-    channel = channels[ch]
+    channel = channels[ch-1]
 
     if mint > 0 and maxt > 0:
         # Reapply original threshold
         channel.getProcessor().setThreshold(mint, maxt, ImageProcessor.NO_LUT_UPDATE)
     else:
-        # Or apply arbitrary threshold
-        depth = channel.getBitDepth()
-        if depth == 16:
-            channel.getProcessor().setThreshold(4000, 65535, ImageProcessor.NO_LUT_UPDATE)
-        elif depth == 8:
-            channel.getProcessor().setThreshold(250, 255, ImageProcessor.NO_LUT_UPDATE)
-        else:
-            IJ.log("Unexpected bit depth ({}-bit), aborting".format(depth))
-            return
-
+        # Or apply automatic threshold
+        threshold = AutoThresholder().getThreshold('MaxEntropy', channel.getProcessor().getHistogram())
+        channel.getProcessor().setThreshold(threshold, 255, ImageProcessor.NO_LUT_UPDATE)
+        
+    # Do a close-open cycle to smooth the mask
+    channel.getProcessor().dilate(1, 0)
+    channel.getProcessor().erode(1, 0)
+    channel.getProcessor().erode(1, 0)
+    channel.getProcessor().dilate(1, 0)
 
     # If a ROI was active on the original image, apply it to the projection
     if roi:
@@ -72,7 +55,7 @@ def run_script():
 
     # Run the particle analyzer
     rt = ResultsTable()
-    analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_OUTLINES | ParticleAnalyzer.DISPLAY_SUMMARY, 0, rt, 0.0, 500.0, 0.0, 1.0)
+    analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_OUTLINES | ParticleAnalyzer.DISPLAY_SUMMARY, 0, rt, 3.0, 50.0, 0.0, 1.0)
     if analyzer.analyze(channel):
         output = analyzer.getOutputImage()
         output.show()
