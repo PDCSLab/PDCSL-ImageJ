2adbb62739b26aa6cc4d3a1c29684ed7495de8b8
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2020-03-27 15:48:10 +0000

    Count foci in the "control" volume.

--- src/main/resources/scripts/Plugins/PDCSL/OC_Foci_Counter.py
+++ src/main/resources/scripts/Plugins/PDCSL/OC_Foci_Counter.py
@@ -13,8 +13,18 @@ from ij.process import ImageStatistics
 from org.incenp.imagej.ChannelMasker import createMasker
 from org.incenp.imagej.Helper import getResultsTable
 
-volumeMasker = createMasker('C:MASK(Moments),G:MASK(Moments),Y:MASK(Moments),R:MASK(MaxEntropy),F:MASK({})'.format(method))
-fociMasker = createMasker('5:APPLY(1),5:APPLY(2),5:APPLY(3),5:APPLY(4)')
+# TODO: We need a better API to facilitate combining multiple masks...
+volumeMasker = \
+    createMasker(# Threshold the different channels
+        'G:MASK(Huang),C:MASK(Moments),G:MASK(Moments),Y:MASK(Moments),R:MASK(MaxEntropy),F:MASK({})'.format(method)).chain(
+    createMasker(# Combine mTurquoise with GFP and Citrine with mCherry
+        '1:COPY(),2:APPLY(3,OR),4:APPLY(5,OR),2:COPY(),3:COPY(),4:COPY(),5:COPY(),6:COPY()').chain(
+    createMasker(# Combine mturquoise/GFP with Citrine/mCherry
+        '1:COPY(),2:APPLY(3,OR),4:COPY(),5:COPY(),6:COPY(),7:COPY(),8:COPY()').chain(
+    createMasker(# Subtract mTurquoise/GFP/Citrine/mCherry from the "total" channel
+        '1:APPLY(2,XOR),3:COPY(),4:COPY(),5:COPY(),6:COPY(),7:COPY()')
+    )))
+fociMasker = createMasker('6:APPLY(1),6:APPLY(2),6:APPLY(3),6:APPLY(4),6:APPLY(5)')
 
 
 def process_image(image, channel_order):
@@ -31,7 +41,7 @@ def process_image(image, channel_order):
     rt = ResultsTable()
     analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE, 0, rt, 3.0, 50.0, 0.0, 1.0)
     
-    for i, channel in enumerate(['mTurquoise', 'GFP', 'Citrine', 'mCherry']):
+    for i, channel in enumerate(['Control', 'mTurquoise', 'GFP', 'Citrine', 'mCherry']):
         foci.setC(i + 1)
         masks.setC(i + 1)
         
