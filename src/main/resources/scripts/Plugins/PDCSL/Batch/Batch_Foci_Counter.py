d1eef62bde4b3b8915a309a9741d29b208d4914a
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2021-02-07 20:25:45 +0000

    Overhaul the Batch_Foci_Counter.
    Partially re-write the Batch_Foci_Counter to bring the following
    improvements:
    - Use the new helper method countParticles;
    - Can work on unprojected images;
    - Can specify the sizes of foci to count.

--- src/main/resources/scripts/Plugins/PDCSL/Batch/Batch_Foci_Counter.py
+++ src/main/resources/scripts/Plugins/PDCSL/Batch/Batch_Foci_Counter.py
@@ -1,39 +1,40 @@
 # @ File(label='Choose a CSV file', style='file') input_file
-# @ String(label='Thresholding method', value='MaxEntropy') method
+# @ String(label='Thresholding method for area', value='Huang') area_thresholder
+# @ String(label='Thresholding method for foci', value='MaxEntropy') foci_thresholder
+# @ Boolean(label='Project Z-stack', value=false) project_stack
+# @ Float(label='Minimum size', value=3.0, min=0, max=500) minimum_size
+# @ Float(label='Maximum size', value=50.0, min=0, max=500) maximum_size
 # @ Boolean(label='Save mask images', value=false, persist=false) save_masks
 
 import os
 
 from ij import IJ
-from ij.measure import ResultsTable
 from ij.plugin import ZProjector
-from ij.plugin.filter import ParticleAnalyzer
-from ij.process import ImageProcessor
-from ij.process import ImageStatistics
+from ij.measure import ResultsTable
 
 from org.incenp.imagej.ChannelMasker import createMasker
 from org.incenp.imagej import Masking
 from org.incenp.imagej import BatchReader
+from org.incenp.imagej.Helper import extractVolumes
+from uk.ac.qmul.bci.pdcsl.imagej.Util import countParticles
 
-masker = createMasker('G:MASK(Huang),F:COPY()').chain(
-         createMasker('1:COPY(),2:MASK({}),2:COPY()'.format(method)).chain(
+
+masker = createMasker('M:MASK({}),S:COPY()'.format(area_thresholder)).chain(
+         createMasker('1:COPY(),2:MASK({}),2:COPY()'.format(foci_thresholder)).chain(
          createMasker('2:APPLY(1),1:COPY(),3:COPY()')))
 
 
-def process_image(image, order, results, savedir=None):
-    projected = ZProjector.run(image, "max")
-    mask = masker.apply(projected, image.getTitle(), order)
+def process_image(image, order, project, sizes, results, savedir=None):
+    if project:
+       image = ZProjector.run(image, "max")
     
-    rt = ResultsTable()
-    analyzer = ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE, 0, rt, 3.0, 50.0, 0.0, 1.0)
-    mask.setC(1)
-    if analyzer.analyze(mask):
-        results.addValue("Foci", rt.getCounter())
-        
-        mask.setC(2)
-        mask.getProcessor().setThreshold(1, 255, ImageProcessor.NO_LUT_UPDATE)
-        stats = ImageStatistics.getStatistics(mask.getProcessor(), ImageStatistics.LIMIT, mask.getCalibration())
-        results.addValue("Area", stats.area)
+    mask = masker.apply(image, image.getTitle(), order)
+    
+    volumes = extractVolumes(mask, [1])
+    foci = countParticles(mask, sizes[0], sizes[1], [1], mask, 0)
+    
+    results.addValue("Foci", foci[0])
+    results.addValue("Volume", volumes[0])
         
     if savedir:
         outname = os.path.join(savedir, os.path.splitext(mask.getTitle())[0]) + '.tiff'
@@ -51,7 +52,7 @@ def run_script():
     while batch.next():
         img = batch.getImage()
         batch.fillResultsTable(results)
-        process_image(img, batch.getCell("Channel Order"), results, savedir=savedir)
+        process_image(img, batch.getCell("Channel Order"), project_stack, (minimum_size, maximum_size), results, savedir=savedir)
         results.show("Batch Foci Counter Results")
         img.close()
         
