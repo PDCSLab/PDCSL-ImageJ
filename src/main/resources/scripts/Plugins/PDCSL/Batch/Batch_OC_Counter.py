5936e0c489d8757dad092d34f498d31114ad6af7
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2021-02-24 09:34:31 +0000

    Update copyright notices.
    Make sure all source files have a copyright notice with the standard
    GPL3 notice.

--- src/main/resources/scripts/Plugins/PDCSL/Batch/Batch_OC_Counter.py
+++ src/main/resources/scripts/Plugins/PDCSL/Batch/Batch_OC_Counter.py
@@ -4,6 +4,23 @@
 # @ String (label='Threshold for non-OC channel', value='MaxEntropy') non_oc_threshold
 # @ Float (label='Minimum size', value=3.0, min=0, max=500) minimum_size
 # @ Float (label='Maximum size', value=50.0, min=0, max=500) maximum_size
+#
+#
+# PDCSL-ImageJ - PDCS Lab’s ImageJ Collection
+# Copyright © 2021 Damien Goutte-Gattat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the Gnu General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
--- src/main/resources/scripts/Plugins/PDCSL/Batch/Batch_Volume_Counter.py
+++ src/main/resources/scripts/Plugins/PDCSL/Batch/Batch_Volume_Counter.py
@@ -1,6 +1,23 @@
 #@ File(label='Choose a CSV file', style='file') input_file
 #@ Boolean(label='Save mask images', value=false, persist=false) save_masks
 #@ Boolean(label='Exclude frames with black masks', value=true, persist=true) exclude_blacks
+#
+#
+# PDCSL-ImageJ - PDCS Lab’s ImageJ Collection
+# Copyright © 2021 Damien Goutte-Gattat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the Gnu General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
